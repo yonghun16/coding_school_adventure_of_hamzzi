cc699dc99881bd0432638f074322f5b94317ab1c
yonghun16 <yonghun16@naver.com>
2023-12-28 04:49:53 +0900

    autoplay update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,55 @@
-/* fade in 이펙트 */
-const fadeEls = document.querySelectorAll('.main .fade-in');
+/* 메인화면 fade in 이펙트 */
+const fadeEls = document.querySelectorAll(".main .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
   // gasp.to(요소, 지속시간, 옵션);
   gsap.to(fadeEl, 1, {
-    delay: (index + 1) * .7,  // 0.7s, 1.4s, 2.1s, 2.7s 딜레이되면서 작동
-    opacity: 1
+    delay: (index + 1) * 0.7, // 0.7s, 1.4s, 2.1s, 2.7s 딜레이되면서 작동
+    opacity: 1,
   });
 });
 
+/* 배경 음악 */
+const btnAudio = document.querySelector(".main .audioBtn");
+const btnPlay = document.querySelector(".main .audioBtnPlay");
+const btnPause = document.querySelector(".main .audioBtnPause");
+const bgm = new Audio();
+let isHideAudioBtn = false;
+bgm.src = "./sound/rmw0027.mp3";
+bgm.loop = true;
+bgm.autoplay = true;
+
+let promise = bgm.play();
+
+if (promise !== undefined) {
+  promise
+    .then((_) => {
+    isHideAudioBtn = !isHideAudioBtn;
+      btnPlay.classList.add("hidden");
+      btnPause.classList.remove("hidden");
+    })
+    .catch((error) => {
+      bgm.autoplay = false;
+    });
+}
+
+btnPlay.onclick = function () {
+  bgm.play();
+};
+
+btnPause.onclick = function () {
+  bgm.pause();
+};
+
+btnAudio.addEventListener("click", function () {
+  isHideAudioBtn = !isHideAudioBtn;
+  if (isHideAudioBtn) {
+    btnPlay.classList.add("hidden");
+    btnPause.classList.remove("hidden");
+  } else {
+    btnPlay.classList.remove("hidden");
+    btnPause.classList.add("hidden");
+  }
+});
 
 /* 라이트 박스 갤러리 */
 function wrap(el) {
@@ -23,37 +65,3 @@ document.querySelectorAll(".pic").forEach((el) => {
 });
 
 refreshFsLightbox();
-
-
-/* 배경 음악 */
-const btnAudio = document.querySelector(".main .audioBtn")
-const btnPlay = document.querySelector(".main .audioBtnPlay");
-const btnPause = document.querySelector(".main .audioBtnPause");
-const bgm = new Audio();
-bgm.src= "./sound/rmw0027.mp3";
-let isHideAudioBtn = false;
-
-// bgm.autoplay = true;
-bgm.loop = true;
-
-// 재생 버튼
-btnPlay.onclick = function () {
-  bgm.play();
-}
-
-// 일시정지 버튼
-btnPause.onclick = function () {
-  bgm.pause();
-}
-
-btnAudio.addEventListener("click", function (){
-  isHideAudioBtn = !isHideAudioBtn;
-  if(isHideAudioBtn) {
-    btnPlay.classList.add('hidden');
-    btnPause.classList.remove('hidden');
-  }
-  else {
-    btnPlay.classList.remove('hidden');
-    btnPause.classList.add('hidden');
-  }
-})
